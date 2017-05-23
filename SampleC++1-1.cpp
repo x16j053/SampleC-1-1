9bd242d56a2d8c1bba24d453382fb222dd81d069
WIN_NT\x16j053 <x16j053@chiba-fjb.ac.jp>
2017-05-23 12:26:46 +0900

    HelloWorld

---
+++ SampleC++1-1.cpp
@@ -0,0 +1,14 @@
+// SampleC++1-1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "HelloWorld" << endl;
+    return 0;
+}
+
